ad1feabd9a298acd62dd63fd7e066113528765c8
Federica Colapaoli <federica.colapaoli@coresecure.com>
2019-11-26 12:31:36 -0500

    readme and communications between apps established

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-    API_ENDPOINT: `https://dashboard.heroku.com/apps/safe-ocean-57145`,
+    // API_ENDPOINT: `https://cors-anywhere.herokuapp.com/https://dashboard.heroku.com/apps/safe-ocean-57145`,
+    API_ENDPOINT: `https://safe-ocean-57145.herokuapp.com/api`,
     TOKEN_KEY: 'bring-it-client-auth-token',
   }
