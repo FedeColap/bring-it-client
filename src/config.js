21ab50e09446c1f1cd17b1a571ad7ea9e8ac57ff
Federica Colapaoli <federica.colapaoli@coresecure.com>
2019-11-26 10:48:21 -0500

    config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: `https://dashboard.heroku.com/apps/safe-ocean-57145`,
     TOKEN_KEY: 'bring-it-client-auth-token',
   }
